c600c3c84e371b8fd219c742b7c3bee9f19fd7f2
hanaheyo <63185597+hanaheyo@users.noreply.github.com>
2020-08-17 12:28:34 +0900

    General changes

--- main.js
+++ main.js
@@ -10,3 +10,7 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", () => {});
